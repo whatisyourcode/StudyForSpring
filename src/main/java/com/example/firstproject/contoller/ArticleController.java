af8adc14096a6db4eb3fe95eefaefbbb99b8494a
whatisyourcode <19941128o@gmail.com>
2024-01-29 00:40:50 +0900

    commit

--- src/main/java/com/example/firstproject/contoller/ArticleController.java
+++ src/main/java/com/example/firstproject/contoller/ArticleController.java
@@ -1,7 +1,9 @@
 package com.example.firstproject.contoller;
 
+import com.example.firstproject.dto.ArticleForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
@@ -10,4 +12,9 @@ public class ArticleController {
         return "articles/new";
     }
 
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm form){
+        System.out.println(form.toString());
+        return "";
+    }
 }
---
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -0,0 +1,21 @@
+package com.example.firstproject.dto;
+
+
+// 이 클래스 파일이
+public class ArticleForm {
+    private String title;
+    private String content;
+
+    public ArticleForm(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "ArticleForm{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
