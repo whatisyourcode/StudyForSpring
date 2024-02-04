2f28b61dacbf76a8645f0f9c3db0ed97e702d22a
whatisyourcode <19941128o@gmail.com>
2024-02-04 22:58:27 +0900

    commit

--- src/main/java/com/example/firstproject/contoller/ArticleController.java
+++ src/main/java/com/example/firstproject/contoller/ArticleController.java
@@ -3,14 +3,15 @@ package com.example.firstproject.contoller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-@Slf4j
+
+import java.util.List;
+
 @Controller
 public class ArticleController {
     @Autowired // 스프링 부트가 미리 생성해 놓은 리파지터리 객체 주입(DI)
@@ -20,33 +21,41 @@ public class ArticleController {
         return "articles/new";
     }
 
-
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){
         // System.out.println(form.toString());
-        log.info(form.toString());
         // 1. DTO를 엔티티로 변환
         Article article = form.toEntity();
-        // System.out.println(article.toString());
-        log.info(article.toString());
+        System.out.println(article.toString());
         // 2. 리파지터리로 엔티티를 DB에 저장
         Article saved = articleRepository.save(article);
-        // System.out.println(saved.toString());
-        log.info(saved.toString());
+        System.out.println(saved.toString());
         return "";
-
     }
-    @GetMapping("/articles/{id}") // 데이터 조회 요청 접수
-    public String show(@PathVariable Long id, Model model){ // 매개변수로 id 받아 오기
-        log.info("id = " + id); // id를 잘 받았는지 확인하는 로그 찍기
-        // 1. id를 조회해 데이터 가져오기
-        // Optional<Article> article = articleRepository.findById(id);
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model){
+        // 1.id를 조회해 DB에서 해당 데이터 가져오기
         Article articleEntity = articleRepository.findById(id).orElse(null);
         // 2. 모델에 데이터 등록하기
         model.addAttribute("article", articleEntity);
-        // 3. 뷰 페이지 반환하기
+        // 3.뷰 페이지 반환하기
         return "articles/show";
+    }
 
+    @GetMapping("/articles")
+    public String index(Model model){
+        // findAll() 반환 타입은 Iterable 이다. 해결 방법은
+        // 1.Iterable 을 List<Article>로 다운캐스팅.
+        // 2.List<Article>을 Iterable 타입으로 맞추기.
+        // 3.ArrayList 이용. -> ArticleRepository에서 findAll() 반환 타입을 ArrayList<> 로 바꾸어 오버라이딩.
+        List<Article> articleEntityList = articleRepository.findAll();
+        model.addAttribute("articleList", articleEntityList);
+
+        // 1. 모든 데이터 가져오기
+        // 2. 모델에 데이터 등록하기
+        // 3. 뷰 페이지 설정하기
+        return "";
     }
 }
 
--- src/main/java/com/example/firstproject/repository/ArticleRepository.java
+++ src/main/java/com/example/firstproject/repository/ArticleRepository.java
@@ -3,7 +3,11 @@ package com.example.firstproject.repository;
 import com.example.firstproject.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 // CrudRepository는 JPA에서 제공하는 인터페이스로 이를 상송해 엔티티를 관리(생성,조회,수정,삭제)할 수 있다.
 public interface ArticleRepository extends CrudRepository<Article, Long> {
+    @Override
+    ArrayList<Article> findAll(); // Iterable -> ArrayList 수정
 
 }
