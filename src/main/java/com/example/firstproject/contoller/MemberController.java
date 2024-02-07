e9b0360938b15a626498590be730fe41a8e88791
hugh <19941128o@gmail.com>
2024-02-07 16:28:04 +0900

    commit

--- src/main/java/com/example/firstproject/contoller/MemberController.java
+++ src/main/java/com/example/firstproject/contoller/MemberController.java
@@ -3,11 +3,17 @@ package com.example.firstproject.contoller;
 import com.example.firstproject.dto.MemberForm;
 import com.example.firstproject.entity.Member;
 import com.example.firstproject.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
+@Slf4j
 @Controller
 public class MemberController {
     @Autowired
@@ -21,11 +27,35 @@ public class MemberController {
     @PostMapping("/join")
     public String joinMember(MemberForm memberForm){
         // System.out.println(memberForm.toString());
+        log.info(memberForm.toString());
         // 1. DTO를 엔티티로 변환
         Member member = memberForm.toEntity();
+        log.info(member.toString());
+        // System.out.println(member.toString());
         // 2. 리파지토리를 이용하여 엔티티를 DB에 저장.
         Member saved = memberRepository.save(member);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
+        //System.out.println(saved.toString());
         return "";
+    }
+
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model){
+        // 1.id를 조회하여 DB에서 해당 데이터 가져오기
+        Member member = memberRepository.findById(id).orElse(null);
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("member", member);
+        // 3. 뷰 페이지 반환하기
+        return "members/show";
+    }
+
+    @GetMapping("/members")
+    public String index(Model model){
+        // 1. 모든 데이터 가져오기
+        List<Member> memberEntityList = memberRepository.findAll();
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("memberList", memberEntityList);
+        // 3. 뷰페이지에 전달하기
+        return "members/index";
     }
 }
--- src/main/java/com/example/firstproject/repository/MemberRepository.java
+++ src/main/java/com/example/firstproject/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package com.example.firstproject.repository;
 import com.example.firstproject.entity.Member;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface MemberRepository extends CrudRepository<Member, Long> {
+    @Override
+    ArrayList<Member> findAll();
 }
