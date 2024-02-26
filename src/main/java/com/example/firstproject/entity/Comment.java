9bf6bdc6edf86055a322d13d40750de996493acd
whatisyourcode <19941128o@gmail.com>
2024-02-27 02:18:09 +0900

    commit

---
+++ src/main/java/com/example/firstproject/entity/Comment.java
@@ -0,0 +1,28 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name="article_id")
+    private Article article; // 외래키
+    @Column
+    private String nickname; // 댓글을 단 사람
+    @Column
+    private String body; // 댓글 본문
+
+
+}
---
+++ src/test/java/com/example/firstproject/repository/CommentRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.example.firstproject.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommentRepositoryTest {
+
+    @Test
+    void findByArticleId() {
+    }
+
+    @Test
+    void findByNickName() {
+    }
+}
