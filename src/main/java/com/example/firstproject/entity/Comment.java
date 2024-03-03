211b3dc5755d7aa39d6c4a2045e6027549c0c895
hugh <19941128o@gmail.com>
2024-03-03 19:26:07 +0900

    commit 15

--- src/main/java/com/example/firstproject/entity/Comment.java
+++ src/main/java/com/example/firstproject/entity/Comment.java
@@ -26,6 +26,17 @@ public class Comment {
     private String body; // 댓글 본문
 
     public static Comment createComment(CommentDto dto, Article article) {
-
+        // 예외 발생
+        if(dto.getId() != null)
+            throw new IllegalArgumentException("댓글 생성 실패! 댓글 id가 없어야 합니다.");
+        if(dto.getArticleId() != article.getId())
+            throw  new IllegalArgumentException("댓글 생성 실패! 게시글의 id가 잘못됐습니다.");
+        // 엔티티 생성 및 반환
+        return new Comment(
+                dto.getId(),
+                article,
+                dto.getNickname(),
+                dto.getBody()
+        );
     }
 }
